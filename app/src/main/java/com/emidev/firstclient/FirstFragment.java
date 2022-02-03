cbb3202dc162c3c70ec4dcc17152a453389be2b2
Emiliano <iacoviola.tb@gmail.com>
2022-02-03 21:58:56 +0100

    GUI improvement

--- app/src/main/java/com/emidev/firstclient/FirstFragment.java
+++ app/src/main/java/com/emidev/firstclient/FirstFragment.java
@@ -1,10 +1,12 @@
 package com.emidev.firstclient;
 
+import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -34,29 +36,46 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+        binding.searchButton.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onClick(View view) {
-                Parser xmlparser = new Parser();
-                Locator location = new Locator();
                 Thread thread = new Thread(new Runnable() {
 
                     @Override
                     public void run() {
                         try  {
-                            String plc = binding.editTextTextPersonName.getText().toString();
+                            Parser xmlparser = new Parser();
+                            Locator location = new Locator();
+                            String plc = binding.insertCityEditText.getText().toString();
                             location.requestLocation(plc, getContext());
                             Place city = (Place) xmlparser.parseDocument(getContext());
-                            String text = city.getName() + " sunrise:" + city.getSunrise() + " sunset:" + city.getSunset();
-                            binding.textviewFirst.setText(text);
-                        } catch (Exception e) {
+                            binding.cityTextview.setVisibility(View.VISIBLE);
+                            binding.cityTextview.setText(city.getName());
+                            binding.countryTextview.setVisibility(View.VISIBLE);
+                            binding.countryTextview.setText(city.getCountry());
+                            binding.latitudeTextview.setVisibility(View.VISIBLE);
+                            binding.latitudeTextview.setText(String.format("%s", city.getLat()));
+                            binding.longitudeTextview.setVisibility(View.VISIBLE);
+                            binding.longitudeTextview.setText(String.format("%s", city.getLng()));
+                            binding.sunriseTextview.setVisibility(View.VISIBLE);
+                            binding.sunriseTextview.setText(city.getSunrise());
+                            binding.sunsetTextview.setVisibility(View.VISIBLE);
+                            binding.sunsetTextview.setText(city.getSunset());
+                            binding.insertCityEditText.setText("");
+                            InputMethodManager imm = (InputMethodManager)getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                            imm.hideSoftInputFromWindow(getActivity().getCurrentFocus().getWindowToken(),InputMethodManager.HIDE_NOT_ALWAYS);                        } catch (Exception e) {
                             e.printStackTrace();
                         }
                     }
                 });
 
                 thread.start();
+                try {
+                    thread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
